fcd3c5ae659521f01f3ff40ec71446d74bda3d69
Thomas Garayua <tgarayua@gmail.com>
2020-09-02 03:04:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
-var katzDeli = [];
+  var num = 0;
 
-function takeANumber(katzDeli, name) {
-  katzDeli.push(`${name}`);
-  return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
+function takeANumber(katzDeli) {
+   num ++;
+  return `Welcome, you are ticket number ${num} in line.`;
 }
 
-
 function nowServing(katzDeli) {
-  let i = 0;
-  while (i < katzDeli.length) {
-    i++;
-  }
   if (katzDeli.length === 0) {
     return "There is nobody waiting to be served!";
   }
-  else
-  return (`Currently serving ${katzDeli.shift()}.`);
+  
+  return `Currently serving ${katzDeli.shift()}.`;
 }
 
-var line = [];
-
 function currentLine(katzDeli) {
+	if (katzDeli.length === 0) {
+  	return "The line is currently empty.";
+  }
+  
+	var line = "The line is currently: ";
+  
   for (let i = 0; i < katzDeli.length; i++) {
-    line.push(` `+[i+1]+`. `  + katzDeli[i])
+  	line += `${i+1}. ${katzDeli[i]}`;
+    
+    // If it's not the last person in line, we want to add a comma
+    if (i < katzDeli.length - 1) {
+    	line += ", ";
+    }
   }
-  if (katzDeli.length === 0) {
-    return "The line is currently empty.";
-  }
+  
+  return line;
+}
